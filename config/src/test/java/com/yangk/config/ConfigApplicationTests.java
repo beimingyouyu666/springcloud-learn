6b6eb17612e5409672350abf160deffabf8c9009
BeiMingYouYu666 <956898043@qq.com>
2020-07-18 16:47:05 +0800

    auto flush config

---
+++ config/src/test/java/com/yangk/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangk.config;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
