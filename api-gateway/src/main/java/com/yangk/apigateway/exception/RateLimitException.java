04fa0365d067a5d67d1ec122738895e8ff2ba9d6
BeiMingYouYu666 <956898043@qq.com>
2020-07-22 07:28:48 +0800

    0722

---
+++ api-gateway/src/main/java/com/yangk/apigateway/exception/RateLimitException.java
@@ -0,0 +1,10 @@
+package com.yangk.apigateway.exception;
+
+/**
+ * @Description
+ * @Author yangkun
+ * @Date 2020/7/22
+ * @Version 1.0
+ */
+public class RateLimitException  extends RuntimeException{
+}
---
+++ api-gateway/src/main/java/com/yangk/apigateway/filter/RateLimitFilter.java
@@ -0,0 +1,46 @@
+package com.yangk.apigateway.filter;
+
+import com.google.common.util.concurrent.RateLimiter;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import com.yangk.apigateway.exception.RateLimitException;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.SERVLET_DETECTION_FILTER_ORDER;
+
+/**
+ * @Description 限流过滤器
+ * @Author yangkun
+ * @Date 2020/7/22
+ * @Version 1.0
+ */
+@Component
+public class RateLimitFilter extends ZuulFilter {
+
+    private static final RateLimiter RATE_LIMITER = RateLimiter.create(100);
+
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return SERVLET_DETECTION_FILTER_ORDER -1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        // 令牌桶算法限流
+        if (!RATE_LIMITER.tryAcquire()) {
+            throw new RateLimitException();
+        }
+        return null;
+    }
+}
